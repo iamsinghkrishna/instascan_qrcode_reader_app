1c1227a921df82c61bbfe854b5dd7ab100bd73b4
skundu <shiladitya.kundu@capgemini.com>
2020-07-07 22:42:07 +0530

    Chrome and IE browser support for Webcam video stream.

--- js/qrreader.js
+++ js/qrreader.js
@@ -2,6 +2,16 @@ var webcam_scanner = document.getElementById('webcam_scanner');
 var scan_result = document.getElementById('scan_result');
 var camera_check = document.getElementById('camera_check');
 
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            webcam_scanner.srcObject = stream;
+        })
+        .catch(function (err0r) {
+            console.error("Something went wrong!");
+        });
+}
+
 var scanner = new Instascan.Scanner({ video: webcam_scanner });
 scanner.addListener('scan', function (content) {
     console.log(content);
@@ -10,7 +20,7 @@ scanner.addListener('scan', function (content) {
 
 Instascan.Camera.getCameras().then(function (cameras) {
     if (cameras.length > 0) {
-        console.error('Cameras found.');
+        console.log('Cameras found.');
         camera_check.innerHTML = 'Cameras found.';
         scanner.start(cameras[0]);
     } else {
