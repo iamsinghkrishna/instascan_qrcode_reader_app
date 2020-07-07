d87ce636fd271aaf957c01c80c3f169430a9aa44
skundu <shiladitya.kundu@capgemini.com>
2020-07-07 13:08:30 +0530

    file added

---
+++ js/qrreader.js
@@ -0,0 +1,22 @@
+var webcam_scanner = document.getElementById('webcam_scanner');
+var scan_result = document.getElementById('scan_result');
+var camera_check = document.getElementById('camera_check');
+
+var scanner = new Instascan.Scanner({ video: document.getElementById('webcam_scanner') });
+scanner.addListener('scan', function (content) {
+    console.log(content);
+    scan_result.innerHTML = content;
+});
+
+Instascan.Camera.getCameras().then(function (cameras) {
+    if (cameras.length > 0) {
+        console.error('Cameras found.');
+        camera_check.innerHTML = 'Cameras found.';
+        scanner.start(cameras[0]);
+    } else {
+        console.error('No cameras found.');
+        camera_check.innerHTML = 'No cameras found.';
+    }
+}).catch(function (e) {
+    console.error(e);
+});
