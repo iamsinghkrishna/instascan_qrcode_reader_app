6a4ff0c9a04adb0a885d558cfbf12a0afae965ee
skundu <shiladitya.kundu@capgemini.com>
2020-07-08 16:48:22 +0530

    live stream stop

--- js/qrreader.js
+++ js/qrreader.js
@@ -7,22 +7,40 @@ var qrOutput = document.getElementById("qrOutput");
 var qrcodeContainer = document.getElementById("qrcodeContainer");
 var qrcodeButton = document.getElementById("qrcodeButton");
 
+var isQrcodeProcessed = false;
+
 //QR code turn on and off switch
 function qrcodeReaderSwitch(){
   if(qrcodeContainer.hidden===true){
     qrcodeContainer.hidden = false;
     qrcodeButton.textContent = "Turn off QR code reader";
-    
+
     // Use facingMode: environment to attemt to get the front camera on phones
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function(stream) {
       video.srcObject = stream;
       video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
       video.play();
+      
+      alert("before requestAnimationFrame ==> isQrcodeProcessed: " + isQrcodeProcessed);
+      
       requestAnimationFrame(tick);
+      
+      alert("after requestAnimationFrame ==> isQrcodeProcessed: " + isQrcodeProcessed);
+      
+      if(isQrcodeProcessed===true){
+        stream.getTracks().forEach(function(track) {
+          if (track.readyState == 'live' && track.kind === 'video') {
+              track.stop();
+          }
+        });
+      }
+      isQrcodeProcessed = false;
+      return;
     });
   }else{
     qrcodeContainer.hidden = true;
     qrcodeButton.textContent = "Turn on QR code reader";
+    return;
   }
 }
 
@@ -55,8 +73,10 @@ function tick() {
       drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
       drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
       qrOutput.innerText = code.data;
+      isQrcodeProcessed = true;
     } else {
       qrOutput.innerText = 'No Data Found!!!';
+      
     }
   }
   requestAnimationFrame(tick);
